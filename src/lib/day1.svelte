cee4734a7daefe07f324f34406844a0ec6a46146
Sofia Merino Costa <scosta.geprix@gmail.com>
2024-04-26 12:55:03 +0100

    ADD: Day 1 - Naughy or Nice [SOLUTION]

--- src/lib/day1.svelte
+++ src/lib/day1.svelte
@@ -1,3 +1,50 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
+
+	let children = [];
+	onMount(async () => {
+		children = await getTallyData();
+	});
+
+	const getTallyData = async () => {
+		const response = await fetch('https://advent.sveltesociety.dev/data/2023/day-one.json');
+		const tallyData = await response.json();
+		return tallyData;
+	};
+</script>
+
 <div>
-    <h1>this is day 1</h1>
+	{#await getTallyData()}
+		<p>
+			<TableBodyCell>Loading...</TableBodyCell>
+		</p>
+	{:then data}
+		<Table>
+			<TableHead>
+				<TableHeadCell>Child name</TableHeadCell>
+				<TableHeadCell>Tally</TableHeadCell>
+				<TableHeadCell>Category</TableHeadCell>
+			</TableHead>
+			<TableBody tableBodyClass="divide-y">
+				{#each data as { name, tally }}
+					<TableBodyRow>
+						<TableBodyCell>{name}</TableBodyCell>
+						<TableBodyCell>{tally}</TableBodyCell>
+						<TableBodyCell style="color: {tally > 0 ? 'green' : 'red'}"
+							>{tally > 0 ? 'Nice' : 'Naughty'}</TableBodyCell
+						>
+					</TableBodyRow>
+				{/each}
+			</TableBody>
+		</Table>
+	{/await}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { Accordion, AccordionItem } from 'flowbite-svelte';
 	import { Footer, FooterCopyright } from 'flowbite-svelte';
-  import { Navbar, NavBrand, NavHamburger } from 'flowbite-svelte';
+	import { Navbar, NavBrand, NavHamburger } from 'flowbite-svelte';
 
 	import Day1 from '$lib/day1.svelte';
 	import Day2 from '$lib/day2.svelte';
@@ -10,17 +10,19 @@
 	const dayComponents = [Day1, Day2];
 </script>
 
-  <!-- NAVBAR -->
-  <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b mb-4">
-    <NavBrand href="https://advent.sveltesociety.dev/#challenges">
-      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Advent of Svelte</span>
-    </NavBrand>
-    <NavHamburger />
-    <span>by <b>sophie-mc-dev</b></span>
-  </Navbar>
-    
-  <!-- MAIN CONTENT -->
-  <main class="flex flex-col items-center p-4 mt-20 mb-10">
+<!-- NAVBAR -->
+<Navbar class="fixed start-0 top-0 z-20 mb-4 w-full border-b px-2 py-2.5 sm:px-4">
+	<NavBrand href="https://advent.sveltesociety.dev/#challenges">
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+			>Advent of Svelte</span
+		>
+	</NavBrand>
+	<NavHamburger />
+	<span>by <b>sophie-mc-dev</b></span>
+</Navbar>
+
+<!-- MAIN CONTENT -->
+<main class="mb-10 mt-20 flex flex-col items-center p-4">
 	<div class="w-full md:w-3/4 lg:w-1/2">
 		<Accordion>
 			{#each Array.from({ length: numberOfDays }, (_, index) => index + 1) as day}
@@ -33,7 +35,7 @@
 	</div>
 </main>
 
-  <!-- FOOTER -->
-	<Footer>
-		<FooterCopyright href="https://github.com/sophie-mc-dev" by="sophie-mc-dev" year={2024} />
-	</Footer>
+<!-- FOOTER -->
+<Footer>
+	<FooterCopyright href="https://github.com/sophie-mc-dev" by="sophie-mc-dev" year={2024} />
+</Footer>
